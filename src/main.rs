abdd94cef5bc2e48caef3d72f8d4d2dcfa82c1b0
Gustavo Maia <gustavopmaia@yahoo.com.br>
2021-08-27 15:06:40 -0300

    Add DELETE request

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@
 pub mod requests;
 use requests::get;
 use requests::post;
+use requests::put;
+use requests::delete;
 
 #[tokio::main]
 async fn main(){
   get("https://httpbin.org/get").await;  
   post("https://httpbin.org/post").await;
+  put("https://httpbin.org/put").await;
+  delete("https://httpbin.org/delete").await;
 }
 
 
--- src/requests.rs
+++ src/requests.rs
@@ -12,6 +12,21 @@ struct Ip {
     ip: String
 }
 
+pub async fn put(url: &str) -> surf::Result<surf::Response> {
+  #[derive(Deserialize, Serialize)]
+    struct Ip {
+        ip: String
+    }
+
+    let uri = url;
+    let data = &Ip {ip: "129.0.0.1".into()};
+    let mut res = surf::put(uri).body_json(data)?.await?;
+    assert_eq!(res.status(), 200);
+    dbg!(res.body_string().await?);
+
+    Ok(res)
+}
+
 pub async fn post(url: &str) -> surf::Result<surf::Response> {
   #[derive(Deserialize, Serialize)]
     struct Ip {
@@ -25,4 +40,19 @@ pub async fn post(url: &str) -> surf::Result<surf::Response> {
     dbg!(res.body_string().await?);
 
     Ok(res)
+}
+
+pub async fn delete(url: &str) -> surf::Result<surf::Response> {
+  #[derive(Deserialize, Serialize)]
+    struct Ip {
+        ip: String
+    }
+
+  let uri = url;
+  let data = &Ip { ip: "129.0.0.1".into() };
+  let mut res = surf::delete(uri).body_json(data)?.await?;
+  assert_eq!(res.status(), 200);
+  dbg!(res.body_string().await?);
+
+  Ok(res)
 }
